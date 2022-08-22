8b3de0fc0eb7deb6327504f19b3d14c0c8ec9a43
marc hoffman <mh@remobjects.com>
2022-08-22 14:36:36 -0400

    DelphiHelpers class for compiler

---
+++ Source/DelphiHelpers.pas
@@ -0,0 +1,23 @@
+﻿namespace RemObjects.Elements.System;
+
+type
+  DelphiHelpers = public static class
+  public
+
+    method CreateInstance(aType: ^Void): ^Void;
+    begin
+      raise new NotImplementedException("DelphiHelpers.CreateInstance is not implemented yet.");
+    end;
+
+    method IsInstanceClass(aInstance: ^Void; aType: ^Void): ^Void;
+    begin
+      raise new NotImplementedException("DelphiHelpers.IsInstanceClass is not implemented yet.");
+    end;
+
+    method IsInstanceInterface(aInstance: ^Void; aType: ^Void): ^Void;
+    begin
+      raise new NotImplementedException("DelphiHelpers.IsInstanceInterface is not implemented yet.");
+    end;
+
+  end;
+end.
