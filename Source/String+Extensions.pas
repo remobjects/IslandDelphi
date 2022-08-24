908c022289281ac5b4e436651f96979a15a30ceb
marc hoffman <mh@remobjects.com>
2022-08-24 11:25:35 -0400

    String conversions

--- Source/String+Extensions.pas
+++ Source/String+Extensions.pas
@@ -6,13 +6,71 @@ type
 
     operator Explicit(aString: IslandString): DelphiUnicodeString;
     begin
-
+      aString:ToDelphiUnicodeString;
     end;
+
+    operator Explicit(aString: IslandString): DelphiWideString;
+    begin
+      aString:ToDelphiAnsiString;
+    end;
+
+    operator Explicit(aString: IslandString): DelphiAnsiString;
+    begin
+      result := aString:ToDelphiWideString;
+    end;
+
+    operator Explicit(aString: IslandString): DelphiShortString;
+    begin
+      result := aString:ToDelphiShortString;
+    end;
+
+    //
 
     operator Explicit(aString: DelphiUnicodeString): IslandString;
     begin
-
+      result := IslandString:FromPChar(aString);
     end;
+
+    operator Explicit(aString: DelphiWideString): IslandString;
+    begin
+      result := IslandString:FromPChar(aString);
+    end;
+
+    operator Explicit(aString: DelphiAnsiString): IslandString;
+    begin
+      result := IslandString:FromPAnsiChars(aString);
+    end;
+
+    operator Explicit(aString: DelphiShortString): IslandString;
+    begin
+      if aString[0] > #0 then
+        result := IslandString:FromPAnsiChars(@(aString[1]));
+    end;
+
+    //
+
+    method ToDelphiUnicodeString: DelphiUnicodeString; inline;
+    begin
+      result := DelphiStringHelpers.DelphiUnicodeStringWithChars(FirstChar, Length);
+    end;
+
+    method ToDelphiWideString: DelphiUnicodeString; inline;
+    begin
+      result := DelphiStringHelpers.ToDelphiWideStringWithChars(FirstChar, Length);
+    end;
+
+    method ToDelphiAnsiString: DelphiAnsiString; inline;
+    begin
+      var lChars := ToAnsiChars(); // ToDo: this extra copy could be optimized with a TPAnsiChar on String?
+      result := DelphiStringHelpers.DelphiAnsiStringWithChars(@lChars[0], Length);
+    end;
+
+    method ToDelphiShortString: DelphiShortString; inline;
+    begin
+      var lChars := ToAnsiChars(); // ToDo: this extra copy could be optimized with a TPAnsiChar on String?
+      result := DelphiStringHelpers.DelphiShortStringWithChars(@lChars[0], Length);
+    end;
+
 
   end;
 
--- Source/Strings.pas
+++ Source/Strings.pas
@@ -1,19 +1,76 @@
 ﻿namespace RemObjects.Elements.System;
 
 type
+  DelphiUnicodeString = public type ^Char;
+  DelphiWideString = public type ^Char;
+  DelphiAnsiString = public type ^AnsiChar;
+  DelphiShortString = public Delphi.System.ShortString;
+
+  {$HIDE H6}
+  {$HIDE H7}
   [Packed]
-  DelphiUnicodeString = public record
-    fStringData: ^Char;
+  DelphiLongStringRecord = record
+    CodePage: UInt16;
+    ElementSize: UInt16;
+    ReferenceCount: UInt32;
+    Length: UInt32;
+    StringData: array[0..0] of Byte;
   end;
 
   [Packed]
-  DelphiWideString = public record
-    fStringData: ^Char;
+  DelphiWideStringRecord = record
+    Length: UInt32;
+    StringData: array[0..0] of Byte;
   end;
 
-  [Packed]
-  DelphiAnsiString = public record
-    fStringData: ^AnsiChar;
+  DelphiStringHelpers = public class
+  public
+
+    class method DelphiUnicodeStringWithChars(aChars: ^Char; aLength: UInt32): DelphiUnicodeString;
+    begin
+      if aLength = 0 then
+        exit nil;
+      var lResult: ^DelphiLongStringRecord := malloc(aLength+sizeOf(DelphiLongStringRecord)+sizeOf(Char));
+      lResult.CodePage := 0; // ToDo
+      lResult.ElementSize := sizeOf(Char);
+      lResult.ReferenceCount := 1;
+      lResult.Length := aLength;
+      memcpy(lResult+sizeOf(DelphiLongStringRecord), aChars, aLength);
+      ^Char(lResult+sizeOf(DelphiLongStringRecord)+aLength)^ := #0;
+    end;
+
+    class method DelphiAnsiStringWithChars(aChars: ^AnsiChar; aLength: UInt32): DelphiAnsiString;
+    begin
+      if aLength = 0 then
+        exit nil;
+      var lResult: ^DelphiLongStringRecord := malloc(aLength+sizeOf(DelphiLongStringRecord)+sizeOf(AnsiChar));
+      lResult.CodePage := 0; // ToDo
+      lResult.ElementSize := sizeOf(AnsiChar);
+      lResult.ReferenceCount := 1;
+      lResult.Length := aLength;
+      memcpy(lResult+sizeOf(DelphiLongStringRecord), aChars, aLength);
+      ^AnsiChar(lResult+sizeOf(DelphiLongStringRecord)+aLength)^ := #0;
+    end;
+
+    class method ToDelphiWideStringWithChars(aChars: ^Char; aLength: UInt32): DelphiUnicodeString;
+    begin
+      if aLength = 0 then
+        exit nil;
+      var lResult := malloc(aLength+sizeOf(UInt32)+sizeOf(Char));
+      memcpy(lResult+sizeOf(UInt32), aChars, aLength);
+      ^UInt32(lResult)^ := aLength;
+      ^Char(lResult+sizeOf(UInt32)+aLength)^ := #0;
+    end;
+
+    class method DelphiShortStringWithChars(aChars: ^AnsiChar; aLength: UInt32): Delphi.System.ShortString;
+    begin
+      if aLength > 255 then
+        raise new ArgumentException("Cannot represent string longer than 255 characters as DelphiShortString");
+      result[0] := AnsiChar(Byte(aLength and $ff));
+      if aLength > 0 then
+        memcpy(@(result[1]), aChars, aLength);
+    end;
+
   end;
 
 end.
