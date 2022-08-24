72015e32877e4b028f4e05ce325a02b7188701f1
marc hoffman <mh@remobjects.com>
2022-08-24 11:37:40 -0400

    Default String override (not enabled)

--- Source/Strings.pas
+++ Source/Strings.pas
@@ -6,6 +6,12 @@ type
   DelphiAnsiString = public type ^AnsiChar;
   DelphiShortString = public Delphi.System.ShortString;
 
+  //{$IF ANSI_STRING}
+  //[assembly:DefaultTypeOverride("String", "Delphi", typeOf(RemObjects.Elements.System.DelphiAnsiString))]
+  //{$ELSE}
+  //[assembly:DefaultTypeOverride("String", "Delphi", typeOf(RemObjects.Elements.System.DelphiUnicodeString))]
+  //{$ENDIF}
+
   {$HIDE H6}
   {$HIDE H7}
   [Packed]
