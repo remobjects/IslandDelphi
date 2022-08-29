bef476acbd813d03096f3155796bbca59dde20b5
marc hoffman <mh@remobjects.com>
2022-08-29 10:36:12 -0400

    MemoryHelpers: optionally don't use DelphiMM yet, for testing

--- Source/MemoryHelpers.pas
+++ Source/MemoryHelpers.pas
@@ -6,12 +6,20 @@ type
 
     method GetMem(aSize: IntPtr): ^Void;
     begin
+      {$IF USE_DELPHI_MM}
       result := :Delphi.System.«@GetMem»(aSize);
+      {$ELSE}
+      result := malloc(aSize);
+      {$ENDIF}
     end;
 
     method FreeMem(aMemory: ^Void);
     begin
+      {$IF USE_DELPHI_MM}
       :Delphi.System.«@FreeMem»(aMemory);
+      {$ELSE}
+      free(aMemory);
+      {$ENDIF}
     end;
 
   end;
