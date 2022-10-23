068b8f5357ecbdcab39258dd239b8d0d46639a9e
Carlo Kok <ck@remobjects.com>
2022-10-23 08:45:31 +0200

    SEH support

--- Source/DelphiHelpers.pas
+++ Source/DelphiHelpers.pas
@@ -20,4 +20,37 @@ type
     end;
 
   end;
+
+  DelphiExceptions = public static class
+  public
+    [RemObjects.Elements.System.GlobalConstructor(0)]
+    class method Initialize;
+    begin
+      ForeignExceptionImplementation.Register(new ForeignExceptionImplementation($EEDFADE, @Handler, @Free));
+    end;
+
+    method Handler(aData: ^Void): Exception;
+    begin
+      {$IFDEF WINDOWS}
+      var aRec := ^rtl.EXCEPTION_RECORD(aData);
+      var b := InternalCalls.Cast<DelphiException>(^Void(aRec^.ExceptionInformation[1]));
+      exit new IslandWrappedDelphiException(b);
+      {$ELSE}
+      var lObj := ^DelphiException(@^CocoaExceptionRecord(aData)^.Object)^;
+      ^DelphiException(@^CocoaExceptionRecord(aData)^.Object)^ := nil;
+      exit new IslandWrappedDelphiException(lObj);
+      {$ENDIF}
+    end;
+
+    method Free(aData: ^Void);
+    begin
+      {$IFDEF WINDOWS}
+      // Nothing to do
+      {$ELSE}
+      var lRec := ^CXXException(aData);
+      lRec := ^CXXException(@^Byte(lRec)[-Int32((^Byte(@lRec^.Unwind) - ^Byte(lRec))) - (sizeOf(IntPtr) * 2)]);
+      Free(lRec)
+      {$ENDIF}
+    end;
+  end;
 end.
