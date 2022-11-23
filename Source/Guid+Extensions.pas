94c954f36043db13999dd556cff66e9f81aa8e37
marc hoffman <mh@remobjects.com>
2022-11-23 15:11:33 -0400

    Removed workaround for E26255

--- Source/Guid+Extensions.pas
+++ Source/Guid+Extensions.pas
@@ -7,23 +7,12 @@ type
   TGUID_Extensions = public extension class(TGUID)
   public
 
-    //operator Implicit(aGuid: TGUID): Guid;
-    //begin
-      //memcpy(@result, @aGuid, sizeOf(Guid));
-    //end;
-
-    //operator Implicit(aGuid: Guid): TGUID;
-    //begin
-      //memcpy(@result, @aGuid, sizeOf(Guid));
-    //end;
-
-    // E26255: Implicit cast operator does not allow explicit cast?
-    operator Explicit(aGuid: TGUID): Guid;
+    operator Implicit(aGuid: TGUID): System.Guid;
     begin
       memcpy(@result, @aGuid, sizeOf(Guid));
     end;
 
-    operator Explicit(aGuid: Guid): TGUID;
+    operator Implicit(aGuid: System.Guid): TGUID;
     begin
       memcpy(@result, @aGuid, sizeOf(Guid));
     end;
