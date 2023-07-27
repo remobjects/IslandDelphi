4b889a1234bf630eb72ee85dd9a9a4eb009063d8
marc hoffman <mh@remobjects.com>
2023-07-27 10:16:15 -0400

    Implemented many Delphi Intrinsic Routines

--- Source/DelphiIntrinsicRoutines.pas
+++ Source/DelphiIntrinsicRoutines.pas
@@ -1,21 +1,21 @@
 ﻿namespace Delphi.System;
 
-function Abs(X: Real): Real; inline;
+function Abs(X: Real): Real; inline; public;
 begin
   result := RemObjects.Elements.System.Math.Abs(X);
 end;
 
-function Abs(X: Int64): Int64; inline;
+function Abs(X: Int64): Int64; inline; public;
 begin
   result := RemObjects.Elements.System.Math.Abs(X);
 end;
 
-function Abs(X: Integer): Integer; inline;
+function Abs(X: Integer): Integer; inline; public;
 begin
   result := RemObjects.Elements.System.Math.Abs(X);
 end;
 
-function Addr(var X): Pointer; inline;
+function Addr(var X): Pointer; inline; public;
 begin
   result := @X;
 end;
@@ -23,86 +23,86 @@ end;
 // assert
 // assigned
 
-function AtomicCmpExchange(var Target: Integer; NewValue: Integer; Comparand: Integer; out Succeeded: Boolean): Integer; inline;
+function AtomicCmpExchange(var Target: Integer; NewValue: Integer; Comparand: Integer; out Succeeded: Boolean): Integer; inline; public;
 begin
   result := interlockedCompareExchange(var Target, Comparand, NewValue)
 end;
 
-function AtomicCmpExchange(var Target: Int64; NewValue: Int64; Comparand: Int64; out Succeeded: Boolean): Int64; inline;
+function AtomicCmpExchange(var Target: Int64; NewValue: Int64; Comparand: Int64; out Succeeded: Boolean): Int64; inline; public;
 begin
   result := interlockedCompareExchange(var Target, Comparand, NewValue)
 end;
 
-function AtomicCmpExchange(var Target: Pointer; NewValue: Pointer; Comparand: Pointer; out Succeeded: Boolean): Pointer; inline;
+function AtomicCmpExchange(var Target: Pointer; NewValue: Pointer; Comparand: Pointer; out Succeeded: Boolean): Pointer; inline; public;
 begin
   result := interlockedCompareExchange(var Target, Comparand, NewValue)
 end;
 
-//function AtomicCmpExchange(var Target; NewValue: Integer; Comparand: Integer; out Succeeded: Boolean): Integer;
-//function AtomicCmpExchange(var Target; NewValue: NativeInt; Comparand: NativeInt; out Succeeded: Boolean): Int64;
-//function AtomicCmpExchange(var Target; NewValue: Pointer>; Comparand: Pointer; out Succeeded: Boolean): Pointer;
+//function AtomicCmpExchange(var Target; NewValue: Integer; Comparand: Integer; out Succeeded: Boolean): Integer; public;
+//function AtomicCmpExchange(var Target; NewValue: NativeInt; Comparand: NativeInt; out Succeeded: Boolean): Int64; public;
+//function AtomicCmpExchange(var Target; NewValue: Pointer>; Comparand: Pointer; out Succeeded: Boolean): Pointer; public;
 
-function AtomicDecrement(var Target: Integer): Integer; inline;
+function AtomicDecrement(var Target: Integer): Integer; inline; public;
 begin
   interlockedDec(var Target);
 end;
 
-function AtomicDecrement(var Target: Int64; Decrement: Int64): Int64; inline;
+function AtomicDecrement(var Target: Int64; Decrement: Int64): Int64; inline; public;
 begin
   interlockedDec(var Target, Decrement);
 end;
 
-function AtomicDecrement(var Target: Pointer; Decrement: NativeInt): Pointer; inline;
+function AtomicDecrement(var Target: Pointer; Decrement: NativeInt): Pointer; inline; public;
 begin
   interlockedDec(var Target, Decrement);
 end;
 
-function AtomicExchange(var Target: Integer; Value: Integer): Integer; inline;
+function AtomicExchange(var Target: Integer; Value: Integer): Integer; inline; public;
 begin
   interlockedExchange(var Target, Value);
 end;
 
-function AtomicExchange(var Target: Int64; Value: Int64): Int64; inline;
+function AtomicExchange(var Target: Int64; Value: Int64): Int64; inline; public;
 begin
   interlockedExchange(var Target, Value);
 end;
 
-function AtomicExchange(var Target: Pointer; Value: Pointer): Pointer; inline;
+function AtomicExchange(var Target: Pointer; Value: Pointer): Pointer; inline; public;
 begin
   interlockedExchange(var Target, Value);
 end;
 
-//function AtomicExchange(var Target; Value: NativeInt): NativeInt;
+//function AtomicExchange(var Target; Value: NativeInt): NativeInt; public;
 //begin
 
 //end;
 
-function AtomicIncrement(var Target: Integer): Integer; inline;
+function AtomicIncrement(var Target: Integer): Integer; inline; public;
 begin
   interlockedInc(var Target);
 end;
 
-function AtomicIncrement(var Target: Integer; Increment: Integer): Integer; inline;
+function AtomicIncrement(var Target: Integer; Increment: Integer): Integer; inline; public;
 begin
   interlockedInc(var Target, Increment);
 end;
 
-function AtomicIncrement(var Target: Int64): Int64; inline;
+function AtomicIncrement(var Target: Int64): Int64; inline; public;
 begin
   interlockedInc(var Target);
 end;
 
-function AtomicIncrement(var Target: Int64; Increment: Int64): Int64; inline;
+function AtomicIncrement(var Target: Int64; Increment: Int64): Int64; inline; public;
 begin
   interlockedInc(var Target, Increment);
 end;
 
-function AtomicIncrement(var Target: Pointer): Pointer; inline;
+function AtomicIncrement(var Target: Pointer): Pointer; inline; public;
 begin
   interlockedInc(var Target);
 end;
 
-function AtomicIncrement(var Target: Pointer; Increment: NativeInt): Pointer; inline;
+function AtomicIncrement(var Target: Pointer; Increment: NativeInt): Pointer; inline; public;
 begin
   interlockedInc(var Target, Increment);
 end;
@@ -111,12 +111,12 @@ end;
 //...
 // chr
 
-function Concat(S1: string; S2: string): string; inline;
+function Concat(S1: string; S2: string): string; inline; public;
 begin
   result := S1+S2;
 end;
 
-function Concat(S1: string; params S2: sequence of string): string;
+function Concat(S1: string; params S2: sequence of String): String; public;
 begin
   var lSB := new StringBuilder;
   lSB.Append(S1);
@@ -125,14 +125,14 @@ begin
   result := lSB.ToString;
 end;
 
-function Concat<T>(S1: array of T; S2: array of T): array of T;
+function Concat<T>(S1: array of T; S2: array of T): array of T; public;
 begin
   result := new array of T(length(S1)+length(S2));
   {$HINT copy}
   raise new NotImplementedException("Concat is not implemented");
 end;
 
-function Concat<T>(S1: array of T; params S2: array of array of T): array of T;
+function Concat<T>(S1: array of T; params S2: array of array of T): array of T; public;
 begin
   var len := length(S1);
   for each s in S2 do
@@ -146,42 +146,42 @@ end;
 //function Copy(S: <string or dynamic array>; Index: Integer; Count: Integer): string;
 // dec
 // default
-procedure Delete(var S: string; &Index: Integer; Count: Integer);
+procedure Delete(var S: string; &Index: Integer; Count: Integer); public;
 begin
   if Count > 0 then
     S := (S as IslandString).Remove(&Index, Count); {$HINT this casts. maybe optimize later?}
 end;
 
-procedure Delete<T>(var S: array of T; &Index: Integer; Count: Integer);
+procedure Delete<T>(var S: array of T; &Index: Integer; Count: Integer); public;
 begin
   raise new NotImplementedException("Delete is not implemented");
 end;
 
 
-//procedure Dispose(var P: Pointer);
+//procedure Dispose(var P: Pointer); public;
 // exclude
 // exit
-//procedure Fail;
-//procedure FillChar(var X; Count: Integer; Value: Ordinal);
-//procedure Finalize(var V; [ Count: NativeUInt]);
-//procedure Finalize(var V; [ Count: NativeUInt]);
+//procedure Fail; public;
+//procedure FillChar(var X; Count: Integer; Value: Ordinal); public;
+//procedure Finalize(var V; [ Count: NativeUInt]); public;
+//procedure Finalize(var V; [ Count: NativeUInt]); public;
 
-procedure FreeMem(var P: Pointer); inline;
+procedure FreeMem(var P: Pointer); inline; public;
 begin
   :Delphi.System.SysFreeMem(P);
 end;
 
-procedure FreeMem(var P: Pointer; Size: Integer);
+procedure FreeMem(var P: Pointer; Size: Integer); public;
 begin
   :Delphi.System.SysFreeMem(P/*, Size*/);
 end;
 
-//procedure GetDir(Drive: Byte; var S: String);
+//procedure GetDir(Drive: Byte; var S: String); public;
 // GetTypeKind
-//procedure Halt([ExitCode: Integer]);
-//procedure Halt([ExitCode: Integer]);
+//procedure Halt([ExitCode: Integer]); public;
+//procedure Halt([ExitCode: Integer]); public;
 
-function Hi(X: Integer): Byte; inline;
+function Hi(X: Integer): Byte; inline; public;
 begin
   result := (Word(X) and $ff00) shr 8;
 end;
@@ -189,19 +189,19 @@ end;
 //high
 //inc
 //include
-//procedure Initialize(var V; [ Count: NativeUInt]);
-//procedure Initialize(var V; [ Count: NativeUInt]);
-procedure Insert(Source: string; var Dest: string; &Index: Integer);
+//procedure Initialize(var V; [ Count: NativeUInt]); public;
+
+procedure Insert(Source: string; var Dest: string; &Index: Integer); public;
 begin
   Dest := (Dest as IslandString).Insert(&Index, Source); {$HINT this casts. maybe optimize later?}
 end;
 
-procedure Insert<T>(Source: array of T; var Dest: array of T; &Index: Integer);
+procedure Insert<T>(Source: array of T; var Dest: array of T; &Index: Integer); public;
 begin
   raise new NotImplementedException("Insert is not implemented");
 end;
 
-function Lo(X: Integer): Byte; inline;
+function Lo(X: Integer): Byte; inline; public;
 begin
   result := Word(X) and $ff;
 end;
@@ -209,28 +209,28 @@ end;
 // IsManagedType
 //length
 //low
-//procedure MemoryBarrier;
+//procedure MemoryBarrier; public;
 
-function MulDivInt64(AValue, AMul, ADiv: Int64): Int64;
+function MulDivInt64(AValue, AMul, ADiv: Int64): Int64; public;
 begin
   MulDivInt64(AValue, AMul, ADiv, out nil);
 end;
 
-function MulDivInt64(AValue, AMul, ADiv: Int64; out Remainder: Int64): Int64;
+function MulDivInt64(AValue, AMul, ADiv: Int64; out Remainder: Int64): Int64; public;
 begin
   raise new NotImplementedException("MulDivInt64 is not implemented");
 end;
 
-//procedure New(var X: Pointer);
+//procedure New(var X: Pointer); public;
 
-function Odd(X: Integer): Boolean; inline;
+function Odd(X: Integer): Boolean; inline; public;
 begin
   result := (X mod 2) ≠ 0
 end;
 
 //ord
 
-function Pi: Extended;
+function Pi: Extended; public;
 begin
   //result := RemObjects.Elements.System.Math.PI;
   raise new NotImplementedException("Pi is not implemented");
@@ -238,90 +238,89 @@ end;
 
 // pred
 
-function Ptr(Address: Integer): Pointer; inline;
+function Ptr(Address: Integer): Pointer; inline; public;
 begin
   result := ^Void(Address);
 end;
 
-//procedure ReallocMem(var P: Pointer; Size: Integer);
+//procedure ReallocMem(var P: Pointer; Size: Integer); public;
 
-function Round(X: Real): Int64;
+function Round(X: Real): Int64; public;
 begin
   result := Int64(RemObjects.Elements.System.Math.Round(X));
 end;
 
-procedure RunError;
+procedure RunError; public;
 begin
   raise new NotImplementedException("RunError is not implemented");
 end;
 
-procedure RunError(ErrorCode: Byte);
+procedure RunError(ErrorCode: Byte); public;
 begin
   raise new NotImplementedException("RunError is not implemented");
 end;
 
 // sizeOf
 
-function Slice(var A: Array; Count: Integer): Pointer;
+function Slice(var A: Array; Count: Integer): Pointer; public;
 begin
   raise new NotImplementedException("Slice is not implemented");
 end;
 
-function Sqr(X: Real): Extended;
+function Sqr(X: Real): Extended; public;
 begin
   raise new NotImplementedException("Sqr is not implemented");
 end;
 
-function Sqr(X: Int64): Int64;
+function Sqr(X: Int64): Int64; public;
 begin
   raise new NotImplementedException("Sqr is not implemented");
 end;
 
-function Sqr(X: Integer): Integer;
+function Sqr(X: Integer): Integer; public;
 begin
   raise new NotImplementedException("Sqr is not implemented");
 end;
 
 //procedure Str(const X [: Width [:Decimals]]; var S: String);
-procedure Str(const X: Object; var S: string);
+procedure Str(const X: Object; var S: string); public;
 begin
   S := X:ToString;
 end;
 
 // succ
 
-function Swap(X: Word/*Integer*/): Word/*Integer*/; inline;
+function Swap(X: Word/*Integer*/): Word/*Integer*/; inline; public;
 begin
   result := Hi(X)+(Lo(X) shl 8)
 end;
 
-
-function Trunc(X: Real): Int64;
+function Trunc(X: Real): Int64; public;
 begin
   result := Int64(RemObjects.Elements.System.Math.Truncate(X));
 end;
 
-//function TypeHandle(T: TypeIdentifier): Pointer;
-//function TypeInfo(T: TypeIdentifier): Pointer;
+//function TypeHandle(T: TypeIdentifier): Pointer; public;
+//function TypeInfo(T: TypeIdentifier): Pointer; public;
 //typeOf
-//procedure Val(S: String; var V; var Code: Integer);
+//procedure Val(S: String; var V; var Code: Integer); public;
 
-procedure VarArrayRedim(var A: Variant; HighBound: Integer);
+procedure VarArrayRedim(var A: Variant; HighBound: Integer); public;
 begin
   raise new NotImplementedException("VarCast is not implemented");
 end;
 
-procedure VarCast(var Dest: Variant; Source: Variant; VarType: Integer);
+procedure VarCast(var Dest: Variant; Source: Variant; VarType: Integer); public;
 begin
   raise new NotImplementedException("VarCast is not implemented");
 end;
 
-procedure VarClear(var V: Variant);
+procedure VarClear(var V: Variant); public;
 begin
   memset(@V, 0, sizeOf(Variant));
 end;
 
-procedure VarCopy(var Dest: Variant; Source: Variant);
+procedure VarCopy(var Dest: Variant; Source: Variant); public;
 begin
   memcpy(@Dest, @Source, sizeOf(Variant));
 end;
