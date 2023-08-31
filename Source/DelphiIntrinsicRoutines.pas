fc4becedbffd02dbe1e08d4b02e1568305c63c7c
marc hoffman <mh@remobjects.com>
2023-08-31 14:55:09 -0400

    case fixes

--- Source/DelphiIntrinsicRoutines.pas
+++ Source/DelphiIntrinsicRoutines.pas
@@ -126,12 +126,12 @@ end;
 //...
 // chr
 
-function Concat(S1: string; S2: string): string; inline; public;
+function Concat(S1: String; S2: String): String; inline; public;
 begin
   result := S1+S2;
 end;
 
-function Concat(S1: string; params S2: sequence of string): string; public;
+function Concat(S1: String; params S2: sequence of String): String; public;
 begin
   var lSB := new StringBuilder;
   lSB.Append(S1);
@@ -171,7 +171,7 @@ end;
 //function Copy(S: <string or dynamic array>; Index: Integer; Count: Integer): string;
 // dec
 // default
-procedure Delete(var S: string; &Index: Integer; Count: Integer); public;
+procedure Delete(var S: String; &Index: Integer; Count: Integer); public;
 begin
   if Count > 0 then
     S := (S as IslandString).Remove(&Index, Count); {$HINT this casts. maybe optimize later?}
@@ -221,7 +221,7 @@ end;
 //include
 //procedure Initialize(var V; [ Count: NativeUInt]); public;
 
-procedure Insert(Source: string; var Dest: string; &Index: Integer); public;
+procedure Insert(Source: String; var Dest: String; &Index: Integer); public;
 begin
   Dest := (Dest as IslandString).Insert(&Index, Source); {$HINT this casts. maybe optimize later?}
 end;
@@ -325,7 +325,7 @@ begin
 end;
 
 //procedure Str(const X [: Width [:Decimals]]; var S: String);
-procedure Str(const X: Object; var S: string); public;
+procedure Str(const X: Object; var S: String); public;
 begin
   S := X:ToString;
 end;
